2bb94307eeefdfbe36e4753af062128b30666b3c
Sankung Sillah <ssillah306@gmail.com>
2021-11-05 19:25:16 -0400

    Project setup

---
+++ src/test/java/com/ssn/inventorymanagementsystem/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssn.inventorymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
