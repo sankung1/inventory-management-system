04dfc52704b6dbc0cb36aab1a4aa665c9637078b
Sankung Sillah <ssillah306@gmail.com>
2021-11-06 21:48:46 -0400

    created a view to the user login page

--- src/main/java/com/ssn/inventorymanagementsystem/controller/UserController.java
+++ src/main/java/com/ssn/inventorymanagementsystem/controller/UserController.java
@@ -2,22 +2,26 @@ package com.ssn.inventorymanagementsystem.controller;
 
 import com.ssn.inventorymanagementsystem.entity.User;
 import com.ssn.inventorymanagementsystem.repository.UserRepository;
+import com.ssn.inventorymanagementsystem.service.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
         @Autowired
-        private UserRepository repo;
+        private UserServices userServices;
 
-        @GetMapping("/users")
-        public String listAll(Model model) {
-            List<User> listUsers = repo.findAll();
-            model.addAttribute("listUsers", listUsers);
-            return "users";
-        }
+        // write http method to save user into the data base
+    @PostMapping("/add")
+    public String add(@RequestBody User user){
+        userServices.saveUser(user);
+        return "New user is Added";
+    }
 }
